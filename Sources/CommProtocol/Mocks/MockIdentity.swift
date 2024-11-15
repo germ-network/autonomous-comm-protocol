5df2cf02939576ea32f773a4846adc861ab86ae1
Mark @ Germ <mark@germ.network>
2024-11-15 10:05:37 -0800

    move Identity Key mock from appCoreLogic

---
+++ Sources/CommProtocol/Mocks/MockIdentity.swift
@@ -0,0 +1,23 @@
+//
+//  MockIdentity.swift
+//  CommProtocol
+//
+//  Created by Anna Mistele on 10/8/24.
+//
+
+import CryptoKit
+
+extension IdentityPrivateKey {
+    public static func mock() throws -> Self {
+        try .init(
+            archive: .init(
+                typedKey: Curve25519.Signing.PrivateKey()
+            ))
+    }
+}
+
+extension IdentityPublicKey {
+    public static func mock() throws -> Self {
+        try IdentityPrivateKey.mock().publicKey
+    }
+}
