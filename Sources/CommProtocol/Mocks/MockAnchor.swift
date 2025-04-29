95f70014d905c39c8baf94df766571d7057bc074
Mark Xue <mark@germ.network>
2025-04-29 14:13:22 -0700

    mock did constructor

--- Sources/CommProtocol/Mocks/MockAnchor.swift
+++ Sources/CommProtocol/Mocks/MockAnchor.swift
@@ -8,22 +8,8 @@
 import CryptoKit
 import Foundation
 
-//extension ATProtoAnchor {
-//	static public func signedMock() throws -> (AnchorPrivateKey, SignedObject<ATProtoAnchor>) {
-//		let anchorPrivateKey = AnchorPrivateKey(algorithm: .curve25519)
-//
-//		let signedObject = try anchorPrivateKey.sign(anchor: .mock())
-//
-//		return (anchorPrivateKey, signedObject)
-//	}
-//}
-//
-//extension ATProtoAnchor {
-//	static public func mock() throws -> ATProtoAnchor {
-//		//ATProto did is hash-bashed, so likely is a digest encoding
-//		.init(
-//			did: .mock(),
-//			previousDigest: nil
-//		)
-//	}
-//}
+extension ATProtoDID {
+	public static func mock() -> ATProtoDID {
+		.init(method: .plc, identifier: "example")
+	}
+}
